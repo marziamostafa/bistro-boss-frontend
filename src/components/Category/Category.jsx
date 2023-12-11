0ec1b7d2d15c2baedfa0b078da60a7e58e431c99
marziamostafa <marziamostafa@gmail.com>
2023-12-12 01:19:08 +0600

    category modified

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -19,16 +19,28 @@ const Category = () => {
                 clickable: true,
             }}
             modules={[Pagination]}
-            className="mySwiper"
+            className="mySwiper mb-16"
         >
             <SwiperSlide>
                 <img src={slide1} alt="" />
-                <h2 className='text-3xl text-center'>Salads</h2>
+                <h2 className='text-3xl text-center -mt-16 text-white'>Salads</h2>
             </SwiperSlide>
-            <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
-            <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
-            <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
-            <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="" />
+                <h2 className='text-3xl text-center -mt-16 text-white'>Pizzas</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="" />
+                <h2 className='text-3xl text-center -mt-16 text-white'>Soups</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide4} alt="" />
+                <h2 className='text-3xl text-center -mt-16 text-white'>Deserts</h2>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide5} alt="" />
+                <h2 className='text-3xl text-center -mt-16 text-white'>Salads</h2>
+            </SwiperSlide>
 
         </Swiper>
     );
