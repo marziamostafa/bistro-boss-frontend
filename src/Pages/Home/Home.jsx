8ebda5274fba127202ffbfcb7b5347aed240cc2f
marziamostafa <marziamostafa@gmail.com>
2023-12-12 01:50:25 +0600

    pupular menu section opened

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner/Banner";
 import Category from "../../components/Category/Category";
+import PopularMenu from "../../components/PopularMenu/PopularMenu";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner />
             <Category />
+            <PopularMenu />
         </div>
     );
 };
---
+++ src/components/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,16 @@
+import SectionTitle from "../SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle
+                heading="From our menu"
+                subHeading="Popolar Items"
+            />
+
+        </section>
+    );
+};
+
+export default PopularMenu;
