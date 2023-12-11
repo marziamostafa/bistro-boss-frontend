490e704e7ebce436f9b88fc00a3929c61e624dfb
marziamostafa <marziamostafa@gmail.com>
2023-12-11 23:10:17 +0600

    Navbar added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -16,6 +16,11 @@ const Footer = () => {
                     </div>
                 </nav>
             </footer>
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+                </aside>
+            </footer>
         </div>
     );
 };
