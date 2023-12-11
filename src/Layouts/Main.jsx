8a5eeb542b3d8af81d6105b62bd73d911d29a706
marziamostafa <marziamostafa@gmail.com>
2023-12-12 02:15:41 +0600

    dynamic menu item file created

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../Pages/Shared/Footer/Footer";
-import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
+import Navbar from "../components/Shared/Navbar/Navbar";
 
 const Main = () => {
     return (
--- src/components/PopularMenu/PopularMenu.jsx
+++ src/components/PopularMenu/PopularMenu.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
 
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const popularItems = data.filter(item => item.category === "popular")
+                setMenu(popularItems)
+            })
+
+    }, [])
+
+    console.log(menu)
+
     return (
         <section>
             <SectionTitle
                 heading="From our menu"
                 subHeading="Popolar Items"
             />
+            <div>
+                {
+                    menu.map(item => (
+                        console.log(item)
+                    ))
+                }
 
+            </div>
         </section>
     );
 };
