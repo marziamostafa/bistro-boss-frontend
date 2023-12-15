d26e7cce1634d48bed2f4b5885cc1a33a9d1b0a9
marziamostafa <marziamostafa@gmail.com>
2023-12-15 23:06:07 +0600

    parallox added in featured memu

--- src/components/FeaturedMenu/FeaturedMenu.jsx
+++ src/components/FeaturedMenu/FeaturedMenu.jsx
@@ -5,12 +5,12 @@ import "../../Styles/FeaturedMenu.css"
 
 const FeaturedMenu = () => {
     return (
-        <div className="featured-item text-white pt-6 my-20">
+        <div className="featured-item text-white pt-6 my-20 bg-fixed">
             <SectionTitle
                 heading="Featured Item"
                 subHeading="Check it out"
             />
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center bg-slate-500 bg-opacity-50 items-center pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -18,7 +18,7 @@ const FeaturedMenu = () => {
                     <p>Dec 14, 2023</p>
                     <p className="uppercase"> Where can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quia nam. Esse molestiae beatae itaque, aut laboriosam, consequuntur voluptate consectetur quo facere assumenda omnis atque nobis, in ducimus vel maxime!</p>.
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4">Order Now</button>
                 </div>
             </div>
 
