f655e765508cd0969caac4dd6e2a77b12c537355
marziamostafa <marziamostafa@gmail.com>
2023-12-15 23:58:54 +0600

    reviews data fetched

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,14 +1,38 @@
 import SectionTitle from "../Shared/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
 
 
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json)
+            .then(data => {
+                setReviews(data)
+            })
+
+    }, [])
     return (
-        <div>
+        <div className="my-20">
             <SectionTitle
                 heading="Testimonials"
                 subHeading="What our client say?"
-
             />
+
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                {
+                    reviews.map(items => (
+
+                ))
+                }
+                <SwiperSlide>Slide 1</SwiperSlide>
+
+            </Swiper>
 
         </div>
     );
