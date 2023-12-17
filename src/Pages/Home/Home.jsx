133a7db8790e584731021b824bb050627b7f6b49
marziamostafa <marziamostafa@gmail.com>
2023-12-18 00:06:49 +0600

    page title added by helmet async

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,14 @@ import Category from "../../components/Category/Category";
 import FeaturedMenu from "../../components/FeaturedMenu/FeaturedMenu";
 import PopularMenu from "../../components/PopularMenu/PopularMenu";
 import Testimonials from "../../components/Testimonials/Testimonials";
-
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner />
             <Category />
             <PopularMenu />
