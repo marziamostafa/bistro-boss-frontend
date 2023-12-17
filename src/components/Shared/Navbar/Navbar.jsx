80d2e442f1fb47eb5459cd372662e38857039d6a
marziamostafa <marziamostafa@gmail.com>
2023-12-17 23:56:30 +0600

    logo and website name added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const navOptions = <>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><a>Item 3</a></li>
     </>
