b4b9799c791504a709dae1691e2a17fd51b2610b
marziamostafa <marziamostafa@gmail.com>
2023-12-19 23:35:24 +0600

    dessert section added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../../components/Shared/Cover/Cover';
 import menuImage from '../../../assets/menu/banner3.jpg'
+import dessertImage from '../../../assets/menu/dessert-bg.jpeg'
 import useMenu from '../../../Hooks/useMenu';
 import SectionTitle from '../../../components/Shared/SectionTitle/SectionTitle';
 import MenuCategory from '../../../components/MenuComponents/MenuCategory/MenuCategory';
@@ -9,10 +10,10 @@ import MenuCategory from '../../../components/MenuComponents/MenuCategory/MenuCa
 
 const Menu = () => {
     const [menu] = useMenu();
-    //const desert = menu.filter(item => item.category === "desert")
-    //const soup = menu.filter(item => item.category === "soup")
-    //const salad = menu.filter(item => item.category === "salad")
-    // const pizza = menu.filter(item => item.category === "pizza")
+    const dessert = menu.filter(item => item.category === "dessert")
+    const soup = menu.filter(item => item.category === "soup")
+    const salad = menu.filter(item => item.category === "salad")
+    const pizza = menu.filter(item => item.category === "pizza")
     const offered = menu.filter(item => item.category === "offered")
     return (
         <div>
@@ -20,17 +21,18 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <Cover
-                bgImg={menuImage}
+            <Cover bgImg={menuImage}
                 title="Our menu"
             />
-            <SectionTitle
-                heading="Today's offer"
-                subHeading="Don't Miss"
-            />
-            <MenuCategory
-                items={offered}
-            />
+
+            {/* offered menu items */}
+            <SectionTitle heading="Today's offer"
+                subHeading="Don't Miss" />
+            <MenuCategory items={offered} />
+
+
+            {/* dessert items */}
+            <MenuCategory items={dessert} title="Dessert" coverImg={dessertImage} />
 
         </div>
     );
--- src/components/MenuComponents/MenuCategory/MenuCategory.jsx
+++ src/components/MenuComponents/MenuCategory/MenuCategory.jsx
@@ -1,17 +1,25 @@
+import Cover from "../../Shared/Cover/Cover";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = ({ items }) => {
+const MenuCategory = ({ items, title, coverImg }) => {
     return (
-        <div className="grid md:grid-cols-2 gap-10 ">
-            {
-                items.map(item => (
-                    <MenuItem
-                        key={item._id}
-                        item={item}
-                    />
-                ))
-            }
+        <div>
+            {title && <Cover
+                bgImg={coverImg}
+                title="Our menu"
+            />}
+            <div className="grid md:grid-cols-2 gap-10 ">
 
+                {
+                    items.map(item => (
+                        <MenuItem
+                            key={item._id}
+                            item={item}
+                        />
+                    ))
+                }
+
+            </div>
         </div>
     );
 };
