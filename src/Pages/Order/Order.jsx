0b90d909e8186796a729065d8a7e8559eda7f674
marziamostafa <marziamostafa@gmail.com>
2023-12-19 23:55:07 +0600

    react tab installed

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,8 +1,21 @@
+import { Helmet } from "react-helmet-async";
+import orderImg from '../../assets/shop/banner2.jpg'
+import Cover from "../../components/Shared/Cover/Cover";
 
 const Order = () => {
     return (
         <div>
-            order
+            <Helmet>
+                <title>Bistro Boss | Order</title>
+            </Helmet>
+            <Cover bgImg={orderImg}
+                title="Order Food"
+            />
+            <div role="tablist" className="tabs tabs-lifted">
+                <a role="tab" className="tab">Tab 1</a>
+                <a role="tab" className="tab tab-active">Tab 2</a>
+                <a role="tab" className="tab">Tab 3</a>
+            </div>
         </div>
     );
 };
