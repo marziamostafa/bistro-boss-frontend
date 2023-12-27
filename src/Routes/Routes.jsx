6a9eff8961568c8b07c87cd08878d29df47e354d
marziamostafa <marziamostafa@gmail.com>
2023-12-27 23:59:44 +0600

    route edited

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -23,7 +23,7 @@ export const router = createBrowserRouter([
 
             },
             {
-                path: "/order",
+                path: "order/soup",
                 element: <Order />
 
             },
