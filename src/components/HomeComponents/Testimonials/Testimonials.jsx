53a3912e4c7ab719a486b5c5e9ba5909b481ab6e
marziamostafa <marziamostafa@gmail.com>
2023-12-27 23:56:20 +0600

    api added to fetch data

--- src/components/HomeComponents/Testimonials/Testimonials.jsx
+++ src/components/HomeComponents/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch('reviews.json')
+        fetch('http://localhost:5000/reviews')
             .then(res => res.json())
             .then(data => {
                 setReviews(data)
@@ -37,7 +37,7 @@ const Testimonials = () => {
                                     value={review.rating}
                                     readOnly
                                 />
-                                <p className="py-8">{review.details}</p>
+                                <p className="py-8">{review.recipe}</p>
                                 <h3 className="text-2xl text-orange-400">{review.name}</h3>
                             </div>
 
