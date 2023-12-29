b9102a838508c44c5192a900777fa26d195fb0fb
marziamostafa <marziamostafa@gmail.com>
2023-12-29 23:18:46 +0600

    adding pagination

--- src/components/OrderComponents/OrderItemTab/OrderItemTab.jsx
+++ src/components/OrderComponents/OrderItemTab/OrderItemTab.jsx
@@ -1,16 +1,40 @@
 import FoodCard from "../FoodCard/FoodCard";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const OrderItemTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    };
     return (
-        <div className="grid md:grid-cols-3 gap-4">
-            {
-                items.map(item => (
-                    <FoodCard
-                        key={item._id}
-                        item={item}
-                    />
-                ))
-            }
+        <div >
+
+
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="grid md:grid-cols-3 gap-4">
+                        {
+                            items.map(item => (
+                                <FoodCard
+                                    key={item._id}
+                                    item={item}
+                                />
+                            ))
+                        }
+                    </div>
+
+                </SwiperSlide>
+
+            </Swiper>
         </div>
     );
 };
