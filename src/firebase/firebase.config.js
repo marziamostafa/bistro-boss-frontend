91c853b1d9b48bd4645214b3a0c4cbf2cd3b07e1
marziamostafa <marziamostafa@gmail.com>
2023-12-30 23:19:48 +0600

    firebase installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCjqe9JbStGk1NYqNPlImvbKg8MlN_9rkE",
+    authDomain: "bistro-boss-b3eec.firebaseapp.com",
+    projectId: "bistro-boss-b3eec",
+    storageBucket: "bistro-boss-b3eec.appspot.com",
+    messagingSenderId: "1016279022596",
+    appId: "1:1016279022596:web:52a219ceb2fe5c60614951"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
