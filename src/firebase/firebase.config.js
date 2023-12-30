5a5ec986c2c6e15624150aeaf9d990477df01493
marziamostafa <marziamostafa@gmail.com>
2023-12-30 23:38:57 +0600

    firebase config setup done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCjqe9JbStGk1NYqNPlImvbKg8MlN_9rkE",
-    authDomain: "bistro-boss-b3eec.firebaseapp.com",
-    projectId: "bistro-boss-b3eec",
-    storageBucket: "bistro-boss-b3eec.appspot.com",
-    messagingSenderId: "1016279022596",
-    appId: "1:1016279022596:web:52a219ceb2fe5c60614951"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
