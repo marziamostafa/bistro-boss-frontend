9724f40f1d69b6165ff4fe2995e1c55e91e6869a
marziamostafa <marziamostafa@gmail.com>
2024-01-02 21:10:41 +0600

    context created

--- src/contextProvider/AuthProvider.jsx
+++ src/contextProvider/AuthProvider.jsx
@@ -1,13 +1,18 @@
 import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
 
 
     const authInfo = {
-        user
+        user,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
