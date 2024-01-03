44f5c41fbb6824c6ebf6695f8066817b6870f1fd
marziamostafa <marziamostafa@gmail.com>
2024-01-04 00:24:05 +0600

    error handling added for each field

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -2,13 +2,13 @@ import { useForm } from "react-hook-form";
 
 
 const SignUp = () => {
-    const { register, handleSubmit, watch, formState: { errors }, } = useForm();
+    const { register, handleSubmit, formState: { errors }, } = useForm();
 
     const onSubmit = (data) => {
         console.log(data)
 
     }
-    console.log(watch("example"))
+
 
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -23,21 +23,23 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required
-                                {...register("name")}
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" {...register("name", { required: true })}
                             />
+                            {errors.name && <span className="text-red-500">Name is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required {...register("email")} />
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" {...register("email", { required: true })} />
+                            {errors.email && <span className="text-red-500">Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name="pasword" placeholder="password" className="input input-bordered" required {...register("password")} />
+                            <input type="password" name="pasword" placeholder="password" className="input input-bordered" {...register("password", { required: true, minLength: 6, maxLength: 20 })} />
+                            {errors.password?.type === 'required' && <span className="text-red-500">Password is required</span>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
