bd50ad2a6067fe32cd523a6950d6237113908d32
marziamostafa <marziamostafa@gmail.com>
2024-01-03 23:59:14 +0600

    data found

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,5 +1,15 @@
+import { useForm } from "react-hook-form";
+
 
 const SignUp = () => {
+    const { register, handleSubmit, watch, formState: { errors }, } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+
+    }
+    console.log(watch("example"))
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -8,24 +18,26 @@ const SignUp = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required
+                                {...register("name")}
+                            />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required {...register("email")} />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name="pasword" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name="pasword" placeholder="password" className="input input-bordered" required {...register("password")} />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
